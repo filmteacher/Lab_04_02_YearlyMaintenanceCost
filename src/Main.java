3cfd1ef931b3ce246d5a52d9ac9f2f8879213df2
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-05-23 16:00:47 -0400

    Finished.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCosts = 149.99;
+        double summerCosts = 84.99;
+        double autumnCosts = 212.86;
+        double winterCosts = 476.87;
+        double totalCosts = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        totalCosts = springCosts + summerCosts + autumnCosts + winterCosts;
+
+        System.out.println("Your home maintenance costs for Spring are: $" + springCosts);
+        System.out.println("Your home maintenance costs for Summer are: $" + summerCosts);
+        System.out.println("Your home maintenance costs for Autumn are: $" + autumnCosts);
+        System.out.println("Your home maintenance costs for Winter are: $" + winterCosts);
+        System.out.println("Your total yearly maintenance cost is: $" + totalCosts);
     }
 }
